b6876295878cbad2b3535cbdff39071f0c53b74d
Damien DUPORTAL <damien.duportal@gmail.com>
2014-12-08 17:58:10 +0100

    wip driver cloudstack
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- drivers/cloudstack/cloudstack.go
+++ drivers/cloudstack/cloudstack.go
@@ -1,102 +1,114 @@
 package cloudstack
 
 import (
-	"fmt"
-	"os/exec"
+    "fmt"
+    "os/exec"
 
-	"github.com/docker/docker/api"
-	flag "github.com/docker/docker/pkg/mflag"
-	"github.com/docker/machine/drivers"
-	"github.com/docker/machine/state"
+    "github.com/docker/docker/api"
+    flag "github.com/docker/docker/pkg/mflag"
+    "github.com/docker/machine/drivers"
+    "github.com/docker/machine/state"
+    gcs "github.com/mindjiver/gopherstack"
 )
 
 // Driver is the driver used when no driver is selected. It is used to
 // connect to existing Docker hosts by specifying the URL of the host as
 // an option.
 type Driver struct {
-	URL string
+    ApiKey      string
+    SecretKey   string
 }
 
 type CreateFlags struct {
-	URL *string
+    ApiKey      *string
+    SecretKey   *string
 }
 
 func init() {
-	drivers.Register("none", &drivers.RegisteredDriver{
-		New:                 NewDriver,
-		RegisterCreateFlags: RegisterCreateFlags,
-	})
+    drivers.Register("cloudstack", &drivers.RegisteredDriver{
+        New:                 NewDriver,
+        RegisterCreateFlags: RegisterCreateFlags,
+    })
 }
 
 // RegisterCreateFlags registers the flags this driver adds to
-// "docker hosts create"
+// "machine create"
 func RegisterCreateFlags(cmd *flag.FlagSet) interface{} {
-	createFlags := new(CreateFlags)
-	createFlags.URL = cmd.String([]string{"-url"}, "", "URL of host when no driver is selected")
-	return createFlags
+    createFlags := new(CreateFlags)
+    createFlags.ApiKey = cmd.String(
+        []string{"-cloudstack-api-key"},
+        "",
+        "Your cloudstack API key"
+    )
+    createFlags.SecretKey = cmd.String(
+        []string{"-cloudstack-secret-key"},
+        "",
+        "Your cloudstack secret key"
+    )
+    return createFlags
 }
 
 func NewDriver(storePath string) (drivers.Driver, error) {
-	return &Driver{}, nil
+    return &Driver{}, nil
 }
 
 func (d *Driver) DriverName() string {
-	return "none"
+    return "cloudstack"
 }
 
 func (d *Driver) SetConfigFromFlags(flagsInterface interface{}) error {
-	flags := flagsInterface.(*CreateFlags)
-	if *flags.URL == "" {
-		return fmt.Errorf("--url option is required when no driver is selected")
-	}
-	url, err := api.ValidateHost(*flags.URL)
-	if err != nil {
-		return err
-	}
-	d.URL = url
-	return nil
+    flags := flagsInterface.(*CreateFlags)
+    if *flags.URL == "" {
+        return fmt.Errorf("--url option is required when no driver is selected")
+    }
+    url, err := api.ValidateHost(*flags.URL)
+    if err != nil {
+        return err
+    }
+    d.URL = url
+    return nil
 }
 
 func (d *Driver) GetURL() (string, error) {
-	return d.URL, nil
+    return d.URL, nil
 }
 
 func (d *Driver) GetIP() (string, error) {
-	return "", nil
+    return "", nil
 }
 
 func (d *Driver) GetState() (state.State, error) {
-	return state.None, nil
+    return state.None, nil
 }
 
 func (d *Driver) Create() error {
-	return nil
+    return nil
 }
 
 func (d *Driver) Start() error {
-	return fmt.Errorf("hosts without a driver cannot be started")
+    return fmt.Warningf("Not implemented yet")
 }
 
-func (d *Driver) Stop() error {
-	return fmt.Errorf("hosts without a driver cannot be stopped")
+func (d *Driver) Stop    () error {
+    return fdsqfds fmt.Errorf ("hosts without a driver cannot be stopped")
 }
 
 func (d *Driver) Remove() error {
-	return nil
+    return nil
 }
 
 func (d *Driver) Restart() error {
-	return fmt.Errorf("hosts without a driver cannot be restarted")
+    return fmt.Errorf("hosts without a driver cannot be restarted")
 }
 
 func (d *Driver) Kill() error {
-	return fmt.Errorf("hosts without a driver cannot be killed")
+    return fmt.Errorf("hosts without a driver cannot be killed")
 }
 
 func (d *Driver) Upgrade() error {
-	return fmt.Errorf("hosts without a driver cannot be upgraded")
+    return fmt.Errorf("hosts without a driver cannot be upgraded")
 }
 
 func (d *Driver) GetSSHCommand(args ...string) (*exec.Cmd, error) {
-	return nil, fmt.Errorf("hosts without a driver do not support SSH")
+    return nil, fmt.Errorf("hosts without a driver do not support SSH")
 }
