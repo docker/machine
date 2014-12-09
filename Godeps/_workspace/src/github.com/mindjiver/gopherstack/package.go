1e3a430c060d57051e53392a7a24bbd44b8d97c5
Damien DUPORTAL <damien.duportal@gmail.com>
2014-12-09 10:47:16 +0000

    Using the cloudstack driver in the full stack
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

---
+++ Godeps/_workspace/src/github.com/mindjiver/gopherstack/package.go
@@ -0,0 +1,5 @@
+// Gopherstack implements some of the Cloudstack 3.0.6 APIs. It has not
+// been test with any newer versions and the main use case is to be
+// used with Packer.io
+
+package gopherstack
