f6b0c819967599cb5180394258744b9c82c49d93
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2014-12-10 15:56:28 +0100

    Add OpenStack driver
    Signed-off-by: Guillaume Giamarchi <guillaume.giamarchi@gmail.com>

--- commands.go
+++ commands.go
@@ -18,6 +18,7 @@ import (
 	_ "github.com/docker/machine/drivers/digitalocean"
 	_ "github.com/docker/machine/drivers/google"
 	_ "github.com/docker/machine/drivers/none"
+	_ "github.com/docker/machine/drivers/openstack"
 	_ "github.com/docker/machine/drivers/virtualbox"
 	_ "github.com/docker/machine/drivers/vmwarefusion"
 	_ "github.com/docker/machine/drivers/vmwarevcloudair"
