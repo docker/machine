eda8bbd2bda93a6dd1009e41bfde8374a79b50b1
Damien DUPORTAL <damien.duportal@gmail.com>
2014-12-11 17:09:13 +0000

    Moving path to filepath dependency
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>
    
    Conflicts:
    	drivers/azure/azure.go
    	store.go
    	store_test.go

--- store_test.go
+++ store_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path"
+	"path/filepath"
 	"testing"
 
 	"github.com/docker/machine/drivers"
@@ -26,7 +27,7 @@ func (d DriverOptionsMock) Bool(key string) bool {
 }
 
 func clearHosts() error {
-	return os.RemoveAll(path.Join(drivers.GetHomeDir(), ".docker/hosts"))
+	return os.RemoveAll(path.Join(drivers.GetHomeDir(), ".docker", "hosts"))
 }
 
 func TestStoreCreate(t *testing.T) {
@@ -49,7 +50,7 @@ func TestStoreCreate(t *testing.T) {
 	if host.Name != "test" {
 		t.Fatal("Host name is incorrect")
 	}
-	path := path.Join(drivers.GetHomeDir(), ".docker/hosts/test")
+	path := filepath.Join(drivers.GetHomeDir(), ".docker", "hosts", "test")
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		t.Fatalf("Host path doesn't exist: %s", path)
 	}
@@ -71,7 +72,7 @@ func TestStoreRemove(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	path := path.Join(drivers.GetHomeDir(), ".docker/hosts/test")
+	path := filepath.Join(drivers.GetHomeDir(), ".docker", "hosts", "test")
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		t.Fatalf("Host path doesn't exist: %s", path)
 	}
