192d8c9843685478ad5e739211858acd64be8d0e
Damien DUPORTAL <damien.duportal@gmail.com>
2014-12-12 14:54:39 +0000

    Moving cloudstack go lib deps to xanzy/go-cloudstack
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- Godeps/_workspace/src/github.com/mindjiver/gopherstack/package.go
+++
@@ -1,5 +0,0 @@
-// Gopherstack implements some of the Cloudstack 3.0.6 APIs. It has not
-// been test with any newer versions and the main use case is to be
-// used with Packer.io
-
-package gopherstack
