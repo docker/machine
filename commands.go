docker/machine

commit b44db203350d91e952730f0000ccd8fc8b16b648

implements a -c/--command flag for ssh
Signed-off-by: Simon Thulbourn <simon+github@thulbourn.com>

commands.go
@@ -41,15 +41,6 @@ func (h HostListItemByName) Less(i, j int) bool {
 	return strings.ToLower(h[i].Name) < strings.ToLower(h[j].Name)
 }
 
-func BeforeHandler(c *cli.Context) error {
-	// if c.Bool("debug") {
-	// 	os.Setenv("DEBUG", "1")
-	// 	initLogging(log.DebugLevel)
-	// }
-
-	return nil
-}
-
 var Commands = []cli.Command{
 	{
 		Name:  "active",
@@ -118,8 +109,6 @@ var Commands = []cli.Command{
 			}
 
 			store := NewStore()
-
-			fmt.Printf("%#v", c.String("url"))
 
 			host, err := store.Create(name, driver, c)
 			if err != nil {
@@ -349,6 +338,13 @@ var Commands = []cli.Command{
 		},
 	},
 	{
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "command, c",
+				Usage: "SSH Command",
+				Value: "",
+			},
+		},
 		Name:  "ssh",
 		Usage: "Log into or run a command on a machine with SSH",
 		Action: func(c *cli.Context) {
@@ -376,7 +372,7 @@ var Commands = []cli.Command{
 				os.Exit(1)
 			}
 
-			sshCmd, err := host.Driver.GetSSHCommand(os.Args[i:]...)
+			sshCmd, err := host.Driver.GetSSHCommand(c.String("command"))
 			if err != nil {
 				log.Errorf("%s", err)
 				os.Exit(1)

commit 6c5da2bc0e1280d18f105b6dd766f0bae2ddfe3b

Fix bug SSH interactive mode
Commit b44db20 introduced a bug that broke
the SSH interactive mode. If no command is
provided, Driver.GetSSHCommand is called with
an empty string instead of no parameter.

Signed-off-by: Guillaume Giamarchi <guillaume.giamarchi@gmail.com>

commands.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 	"sync"
 	"text/tabwriter"
@@ -373,7 +374,12 @@ var Commands = []cli.Command{
 				os.Exit(1)
 			}
 
-			sshCmd, err := host.Driver.GetSSHCommand(c.String("command"))
+			var sshCmd *exec.Cmd
+			if c.String("command") == "" {
+				sshCmd, err = host.Driver.GetSSHCommand()
+			} else {
+				sshCmd, err = host.Driver.GetSSHCommand(c.String("command"))
+			}
 			if err != nil {
 				log.Errorf("%s", err)
 				os.Exit(1)