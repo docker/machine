fb5036e8fdbdae9879502aa8ed170659670b37c0
Evan Hazlett <ejhazlett@gmail.com>
2015-01-27 01:04:33 -0500

    generic: add precreatecheck to driver
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- drivers/generic/generic.go
+++ drivers/generic/generic.go
@@ -90,6 +90,10 @@ func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
 	return nil
 }
 
+func (d *Driver) PreCreateCheck() error {
+	return nil
+}
+
 func (d *Driver) Create() error {
 	log.Infof("Importing SSH key...")
 
