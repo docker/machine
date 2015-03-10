448f306c564c1584ddaa9fd0777a93187f108393
Simon Thulbourn <simon+github@thulbourn.com>
2015-03-10 18:17:03 +0000

    change host to use new code
    Signed-off-by: Simon Thulbourn <simon+github@thulbourn.com>

--- host.go
+++ host.go
@@ -24,7 +24,7 @@ import (
 	"github.com/docker/machine/state"
 	"github.com/docker/machine/utils"
 
-	mos "github.com/docker/machine/machine/os"
+	"github.com/docker/machine/host/provision"
 )
 
 var (
