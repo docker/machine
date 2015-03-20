b567490e95a68def09b59f7e1e8400ebdc778454
Ke Zhu <kzhu@us.ibm.com>
2015-03-20 09:13:18 -0400

    fix option --virtualbox-boot2docker-url
    Signed-off-by: Ke Zhu <kzhu@us.ibm.com>

--- drivers/virtualbox/virtualbox.go
+++ drivers/virtualbox/virtualbox.go
@@ -183,7 +183,7 @@ func (d *Driver) Create() error {
 	if d.Boot2DockerURL != "" {
 		isoURL = d.Boot2DockerURL
 		log.Infof("Downloading %s from %s...", isoFilename, isoURL)
-		if err := b2dutils.DownloadISO(commonIsoPath, isoFilename, isoURL); err != nil {
+		if err := b2dutils.DownloadISO(d.storePath, isoFilename, isoURL); err != nil {
 			return err
 
 		}
