cb5de03fcb905effa5b14c17ca28f57f815ff0e2
Luke Roberts <luke@sqwiggle.com>
2015-03-25 21:00:29 -0700

    indicate a successful removal of a machine
    Signed-off-by: Luke Roberts <luke@speak.io>

--- commands.go
+++ commands.go
@@ -653,6 +653,8 @@ func cmdRm(c *cli.Context) {
 	}
 	if isError {
 		log.Fatal("There was an error removing a machine. To force remove it, pass the -f option. Warning: this might leave it running on the provider.")
+	} else {
+		log.Print("The machine was successfully removed")
 	}
 }
 
