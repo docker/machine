2c54f32fc39a9f765db83cb87d278e324f0cf199
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-04-02 14:25:42 +0200

    Initialize Scaleway driver
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- commands.go
+++ commands.go
@@ -24,6 +24,7 @@ import (
 	_ "github.com/docker/machine/drivers/none"
 	_ "github.com/docker/machine/drivers/openstack"
 	_ "github.com/docker/machine/drivers/rackspace"
+	_ "github.com/docker/machine/drivers/scaleway"
 	_ "github.com/docker/machine/drivers/softlayer"
 	_ "github.com/docker/machine/drivers/virtualbox"
 	_ "github.com/docker/machine/drivers/vmwarefusion"
