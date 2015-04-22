a0f079a627d6ff908d2076b065090251c2ec67dd
Dave Henderson <Dave.Henderson@ca.ibm.com>
2015-04-21 22:58:07 -0400

    Enhancing `docker-machine ip` to support multiple host arguments
    Fixes #999
    
    Signed-off-by: Dave Henderson <Dave.Henderson@ca.ibm.com>

--- commands/ip.go
+++ commands/ip.go
@@ -1,18 +1,13 @@
 package commands
 
 import (
-	"fmt"
-
 	log "github.com/Sirupsen/logrus"
 
 	"github.com/codegangsta/cli"
 )
 
 func cmdIp(c *cli.Context) {
-	ip, err := getHost(c).Driver.GetIP()
-	if err != nil {
+	if err := runActionWithContext("ip", c); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(ip)
 }
