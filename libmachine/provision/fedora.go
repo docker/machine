584dec8a7465d94ee99584d30adad0534a39707f
Evan Hazlett <ejhazlett@gmail.com>
2015-05-28 08:51:42 -0400

    redhat: use engine version 1.7rc1
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- libmachine/provision/fedora.go
+++ libmachine/provision/fedora.go
@@ -8,7 +8,7 @@ const (
 	// TODO: eventually the RPM install process will be integrated
 	// into the get.docker.com install script; for now
 	// we install via vendored RPMs
-	dockerFedoraRPMPath = "https://docker-mcn.s3.amazonaws.com/public/fedora/rpms/docker-engine-1.6.1-0.0.20150511.171646.git1b47f9f.fc21.x86_64.rpm"
+	dockerFedoraRPMPath = "https://test.docker.com/rpm/1.7.0-rc1/fedora-21/RPMS/x86_64/docker-engine-1.7.0-0.1.rc1.fc21.x86_64.rpm"
 )
 
 func init() {
