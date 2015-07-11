8a77935cd4048a4ce33a502ef5c3e3b193d64fc6
Eric Sage <eric.david.sage@gmail.com>
2015-07-11 00:49:59 -0400

    Return better error for issue #1502.
    Signed-off-by: Eric Sage <eric.david.sage@gmail.com>

--- commands/ls.go
+++ commands/ls.go
@@ -87,6 +87,9 @@ func parseFilters(filters []string) (FilterOptions, error) {
 	options := FilterOptions{}
 	for _, f := range filters {
 		kv := strings.SplitN(f, "=", 2)
+		if len(kv) != 2 {
+			return options, fmt.Errorf("Unupported filter syntax, please use help ls for details on correct usage.")
+		}
 		key, value := kv[0], kv[1]
 
 		switch key {
