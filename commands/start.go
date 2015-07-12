1059bd4959d6dc15e72c7d8017d4a24a9164988b
Dave Henderson <dhenderson@gmail.com>
2015-07-12 07:42:27 -0400

    Just informing the user that re-eval may be necessary, instead of going to the trouble of trying to detect IP changes
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- commands/start.go
+++ commands/start.go
@@ -10,4 +10,5 @@ func cmdStart(c *cli.Context) {
 	if err := runActionWithContext("start", c); err != nil {
 		log.Fatal(err)
 	}
+	log.Info("Started machines may have new IP addresses. You may need to re-run the `docker-machine env` command.")
 }
