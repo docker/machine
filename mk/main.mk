3341c4e61ed56edf20de77c80a9bc2f448283542
Olivier Gambier <olivier@docker.com>
2015-10-20 13:18:02 -0700

    Minor cleanup and fix #2022
    Signed-off-by: Olivier Gambier <olivier@docker.com>

--- mk/main.mk
+++ mk/main.mk
@@ -3,7 +3,7 @@ GO_LDFLAGS := -X `go list ./version`.GitCommit=`git rev-parse --short HEAD`
 GO_GCFLAGS :=
 
 # Full package list
-PKGS := $(shell go list -tags "$(BUILDTAGS)" ./commands/... ./libmachine/... ./drivers/... | grep -v "/vendor/" | grep -v "/Godeps/")
+PKGS := $(shell go list -tags "$(BUILDTAGS)" ./... | grep -v "/vendor/" | grep -v "/Godeps/" | grep -v "/cmd")
 
 # Support go1.5 vendoring (let us avoid messing with GOPATH or using godep)
 export GO15VENDOREXPERIMENT = 1
@@ -32,7 +32,6 @@ VERBOSE_GO :=
 GO := go
 ifeq ($(VERBOSE),true)
 	VERBOSE_GO := -v
-	GO := go
 endif
 
 include mk/build.mk
@@ -58,9 +57,7 @@ plugins: build-plugins
 cross: build-x
 
 clean: coverage-clean build-clean
-test: dco fmt vet test-short
+test: dco fmt test-short vet
 validate: dco fmt vet lint test-short test-long
-install:
-	cp ./bin/docker-machine* /usr/local/bin/
 
 .PHONY: .all_build .all_coverage .all_release .all_test .all_validate test build validate clean
