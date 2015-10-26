bc273bdc04926019bad58d29bc439f858a521010
David Gageot <david@gageot.net>
2015-10-26 18:46:58 +0100

    Revert "Remove duplication over GetIP"
    This reverts commit 99aacc7b79bf24fda123213bb0a84d8d7b59f28d.
    
    Signed-off-by: David Gageot <david@gageot.net>

--- drivers/generic/generic.go
+++ drivers/generic/generic.go
@@ -121,7 +121,11 @@ func (d *Driver) GetURL() (string, error) {
 	return fmt.Sprintf("tcp://%s:2376", ip), nil
 }
 
+func (d *Driver) GetIP() (string, error) {
+	if d.IPAddress == "" {
+		return "", fmt.Errorf("IP address is not set")
 	}
+	return d.IPAddress, nil
 }
 
 func (d *Driver) GetState() (state.State, error) {
