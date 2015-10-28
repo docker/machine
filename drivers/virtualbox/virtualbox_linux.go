42f1dbb9643dd681a1de9c58b49c2dc1056a450e
Emmanuel Bretelle <chantr4@gmail.com>
2015-10-27 22:13:30 -0700

    virtualbox: support linux host
    Fixes #2110
    
    Signed-off-by: Emmanuel Bretelle <chantr4@gmail.com>

--- drivers/virtualbox/virtualbox_linux.go
+++ drivers/virtualbox/virtualbox_linux.go
@@ -1,7 +1,21 @@
 package virtualbox
 
+import (
+	"strings"
+
+	"github.com/docker/machine/libmachine/log"
+)
+
 // IsVTXDisabled checks if VT-X is disabled in the BIOS. If it is, the vm will fail to start.
 // If we can't be sure it is disabled, we carry on and will check the vm logs after it's started.
 func (d *Driver) IsVTXDisabled() bool {
-	return false
+	errmsg := "Couldn't check that VT-X/AMD-v is enabled. Will check that the vm is properly created: %v"
+	output, err := cmdOutput("grep", "vmx", "/proc/cpuinfo")
+	if err != nil {
+		log.Debugf(errmsg, err)
+		return false
+	}
+
+	disabled := !strings.Contains(output, "vmx")
+	return disabled
 }
