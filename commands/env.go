f30bb39a320be9d0c3fec2f9e11c2a3fd94f0fcf
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-11-10 14:41:07 +0100

    Fix #2204 broken env unset
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- commands/env.go
+++ commands/env.go
@@ -17,7 +17,8 @@ const (
 )
 
 var (
-	errImproperEnvArgs = errors.New("Error: Expected either one machine name, or -u flag to unset the variables in the arguments")
+	errImproperEnvArgs      = errors.New("Error: Expected one machine name")
+	errImproperUnsetEnvArgs = errors.New("Error: Expected no machine name when the -u flag is present")
 )
 
 type ShellConfig struct {
@@ -38,7 +39,14 @@ func cmdEnv(c CommandLine) error {
 	// being run (it is intended to be run in a subshell)
 	log.SetOutWriter(os.Stderr)
 
-	if len(c.Args()) != 1 && !c.Bool("unset") {
+	if c.Bool("unset") {
+		return unset(c)
+	}
+	return set(c)
+}
+
+func set(c CommandLine) error {
+	if len(c.Args()) != 1 {
 		return errImproperEnvArgs
 	}
 
@@ -52,24 +60,16 @@ func cmdEnv(c CommandLine) error {
 		return fmt.Errorf("Error running connection boilerplate: %s", err)
 	}
 
-	userShell := c.String("shell")
-	if userShell == "" {
-		shell, err := detectShell()
-		if err != nil {
-			return err
-		}
-		userShell = shell
+	userShell, err := getShell(c)
+	if err != nil {
+		return err
 	}
-
-	t := template.New("envConfig")
-
-	usageHint := generateUsageHint(userShell, os.Args)
 
 	shellCfg := &ShellConfig{
 		DockerCertPath:  filepath.Join(mcndirs.GetMachineDir(), host.Name),
 		DockerHost:      dockerHost,
 		DockerTLSVerify: "1",
-		UsageHint:       usageHint,
+		UsageHint:       generateUsageHint(userShell, os.Args),
 		MachineName:     host.Name,
 	}
 
@@ -79,61 +79,20 @@ func cmdEnv(c CommandLine) error {
 			return fmt.Errorf("Error getting host IP: %s", err)
 		}
 
-		// first check for an existing lower case no_proxy var
-		noProxyVar := "no_proxy"
-		noProxyValue := os.Getenv("no_proxy")
-
-		// otherwise default to allcaps HTTP_PROXY
-		if noProxyValue == "" {
-			noProxyVar = "NO_PROXY"
-			noProxyValue = os.Getenv("NO_PROXY")
-		}
+		noProxyVar, noProxyValue := findNoProxyFromEnv()
 
 		// add the docker host to the no_proxy list idempotently
 		switch {
 		case noProxyValue == "":
 			noProxyValue = ip
 		case strings.Contains(noProxyValue, ip):
-			//ip already in no_proxy list, nothing to do
+		//ip already in no_proxy list, nothing to do
 		default:
 			noProxyValue = fmt.Sprintf("%s,%s", noProxyValue, ip)
 		}
 
 		shellCfg.NoProxyVar = noProxyVar
 		shellCfg.NoProxyValue = noProxyValue
-	}
-
-	// unset vars
-	if c.Bool("unset") {
-		switch userShell {
-		case "fish":
-			shellCfg.Prefix = "set -e "
-			shellCfg.Delimiter = ""
-			shellCfg.Suffix = ";\n"
-		case "powershell":
-			shellCfg.Prefix = "Remove-Item Env:\\\\"
-			shellCfg.Delimiter = ""
-			shellCfg.Suffix = "\n"
-		case "cmd":
-			// since there is no way to unset vars in cmd just reset to empty
-			shellCfg.DockerCertPath = ""
-			shellCfg.DockerHost = ""
-			shellCfg.DockerTLSVerify = ""
-			shellCfg.Prefix = "set "
-			shellCfg.Delimiter = "="
-			shellCfg.Suffix = "\n"
-		default:
-			shellCfg.Prefix = "unset "
-			shellCfg.Delimiter = " "
-			shellCfg.Suffix = "\n"
-		}
-
-		tmpl, err := t.Parse(envTmpl)
-		if err != nil {
-			return err
-		}
-
-		return tmpl.Execute(os.Stdout, shellCfg)
 	}
 
 	switch userShell {
@@ -155,12 +114,78 @@ func cmdEnv(c CommandLine) error {
 		shellCfg.Delimiter = "=\""
 	}
 
+	return executeTemplateStdout(shellCfg)
+}
+
+func unset(c CommandLine) error {
+	if len(c.Args()) != 0 {
+		return errImproperUnsetEnvArgs
+	}
+
+	userShell, err := getShell(c)
+	if err != nil {
+		return err
+	}
+
+	shellCfg := &ShellConfig{
+		UsageHint: generateUsageHint(userShell, os.Args),
+	}
+
+	if c.Bool("no-proxy") {
+		shellCfg.NoProxyVar, shellCfg.NoProxyValue = findNoProxyFromEnv()
+	}
+
+	switch userShell {
+	case "fish":
+		shellCfg.Prefix = "set -e "
+		shellCfg.Suffix = ";\n"
+		shellCfg.Delimiter = ""
+	case "powershell":
+		shellCfg.Prefix = `Remove-Item Env:\\`
+		shellCfg.Suffix = "\n"
+		shellCfg.Delimiter = ""
+	case "cmd":
+		shellCfg.Prefix = "SET "
+		shellCfg.Suffix = "\n"
+		shellCfg.Delimiter = "="
+	default:
+		shellCfg.Prefix = "unset "
+		shellCfg.Suffix = "\n"
+		shellCfg.Delimiter = ""
+	}
+
+	return executeTemplateStdout(shellCfg)
+}
+
+func executeTemplateStdout(shellCfg *ShellConfig) error {
+	t := template.New("envConfig")
 	tmpl, err := t.Parse(envTmpl)
 	if err != nil {
 		return err
 	}
 
 	return tmpl.Execute(os.Stdout, shellCfg)
+}
+
+func getShell(c CommandLine) (string, error) {
+	userShell := c.String("shell")
+	if userShell != "" {
+		return userShell, nil
+	}
+	return detectShell()
+}
+
+func findNoProxyFromEnv() (string, string) {
+	// first check for an existing lower case no_proxy var
+	noProxyVar := "no_proxy"
+	noProxyValue := os.Getenv("no_proxy")
+
+	// otherwise default to allcaps HTTP_PROXY
+	if noProxyValue == "" {
+		noProxyVar = "NO_PROXY"
+		noProxyValue = os.Getenv("NO_PROXY")
+	}
+	return noProxyVar, noProxyValue
 }
 
 func generateUsageHint(userShell string, args []string) string {
