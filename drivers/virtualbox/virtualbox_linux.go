f9b11d7139c4272d05377abf1d04c089a454da4f
Emmanuel Bretelle <chantr4@gmail.com>
2015-11-12 23:42:28 -0500

    [linux] virtualization detection
    Use ioutil and also check for smd flag
    
    Signed-off-by: Emmanuel Bretelle <chantr4@gmail.com>

--- drivers/virtualbox/virtualbox_linux.go
+++ drivers/virtualbox/virtualbox_linux.go
@@ -1,21 +1,32 @@
 package virtualbox
 
 import (
-	"strings"
+	"bytes"
+	"io/ioutil"
 
 	"github.com/docker/machine/libmachine/log"
 )
 
 // IsVTXDisabled checks if VT-X is disabled in the BIOS. If it is, the vm will fail to start.
 // If we can't be sure it is disabled, we carry on and will check the vm logs after it's started.
+// We want to check that either vmx or smd flags are present in /proc/cpuinfo.
 func (d *Driver) IsVTXDisabled() bool {
+	features := [2][]byte{
+		{'v', 'm', 'x'},
+		{'s', 'm', 'd'},
+	}
+
 	errmsg := "Couldn't check that VT-X/AMD-v is enabled. Will check that the vm is properly created: %v"
-	output, err := cmdOutput("grep", "vmx", "/proc/cpuinfo")
+	content, err := ioutil.ReadFile("/proc/cpuinfo")
 	if err != nil {
 		log.Debugf(errmsg, err)
 		return false
 	}
 
-	disabled := !strings.Contains(output, "vmx")
-	return disabled
+	for _, v := range features {
+		if bytes.Contains(content, v) {
+			return false
+		}
+	}
+	return true
 }
