404953e6e35b9a4a05500f1e4a50b90a61839ea4
Elliot Anderson <elliot.a@gmail.com>
2015-11-20 15:14:56 +1000

    Make user supplied input more obvious
    The command example given does not make it obvious that `name` is to be supplied by the user. Add square bracket notation around it to indicate the need to replace it.

--- commands/config.go
+++ commands/config.go
@@ -21,7 +21,7 @@ type ErrCertInvalid struct {
 
 func (e ErrCertInvalid) Error() string {
 	return fmt.Sprintf(`There was an error validating certificates for host %q: %s
-You can attempt to regenerate them using 'docker-machine regenerate-certs name'.
+You can attempt to regenerate them using 'docker-machine regenerate-certs [name]'.
 Be advised that this will trigger a Docker daemon restart which will stop running containers.
 `, e.hostURL, e.wrappedErr)
 }
