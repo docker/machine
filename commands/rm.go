77fba72d13edd1b7964c2dbeeedbe190f1831ae8
Anil Belur <askb23@gmail.com>
2015-11-24 21:34:33 +0530

    Fixes #2349 - rm get user confirmation before proceeding further
    * adds new flag `-y` prompting for user confirmation before removal
    * Modified existing integration tests to work with the fix #2349.
    * Added tests for checking user confirmation, updated the test cases use sub-shell with `|`
    * Updated the reference docs for rm sub-command
    
    incorporated changes by @dgageot, @jeanlaurent and @nathanleclaire
    
    Signed-off-by: Anil Belur <askb23@gmail.com>

--- commands/rm.go
+++ commands/rm.go
@@ -14,11 +14,19 @@ func cmdRm(c CommandLine, api libmachine.API) error {
 	}
 
 	force := c.Bool("force")
+	confirm := c.Bool("y")
 
 	for _, hostName := range c.Args() {
 		h, err := api.Load(hostName)
 		if err != nil {
 			return fmt.Errorf("Error removing host %q: %s", hostName, err)
+		}
+
+		if !confirm && !force {
+			userinput, err := confirmInput(fmt.Sprintf("Do you really want to remove %q?", hostName))
+			if !userinput {
+				return err
+			}
 		}
 
 		if err := h.Driver.Remove(); err != nil {
