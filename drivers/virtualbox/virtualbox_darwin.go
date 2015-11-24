c82af649dc6cc086b04e37fbaae18ac84c05ad93
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-11-24 16:00:43 +0100

    Add Test on VTX Detection
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- drivers/virtualbox/virtualbox_darwin.go
+++ drivers/virtualbox/virtualbox_darwin.go
@@ -10,15 +10,16 @@ import (
 // IsVTXDisabled checks if VT-X is disabled in the BIOS. If it is, the vm will fail to start.
 // If we can't be sure it is disabled, we carry on and will check the vm logs after it's started.
 func (d *Driver) IsVTXDisabled() bool {
-	errmsg := "Couldn't check that VT-X/AMD-v is enabled. Will check that the vm is properly created: %v"
 	features, err := syscall.Sysctl("machdep.cpu.features")
 	if err != nil {
-		log.Debugf(errmsg, err)
+		log.Debugf("Couldn't check that VT-X/AMD-v is enabled. Will check that the vm is properly created: %v", err)
 		return false
 	}
+	return isVTXDisabled(features)
+}
 
-	disabled := !strings.Contains(features, "VMX")
-	return disabled
+func isVTXDisabled(features string) bool {
+	return !strings.Contains(features, "VMX")
 }
 
 func detectVBoxManageCmd() string {
--- drivers/virtualbox/virtualbox_linux.go
+++ drivers/virtualbox/virtualbox_linux.go
@@ -11,20 +11,21 @@ import (
 // If we can't be sure it is disabled, we carry on and will check the vm logs after it's started.
 // We want to check that either vmx or smd flags are present in /proc/cpuinfo.
 func (d *Driver) IsVTXDisabled() bool {
+	cpuinfo, err := ioutil.ReadFile("/proc/cpuinfo")
+	if err != nil {
+		log.Debugf("Couldn't check that VT-X/AMD-v is enabled. Will check that the vm is properly created: %v", err)
+		return false
+	}
+	return isVTXDisabled(cpuinfo)
+}
+
+func isVTXDisabled(cpuinfo []byte) bool {
 	features := [2][]byte{
 		{'v', 'm', 'x'},
 		{'s', 'm', 'd'},
 	}
-
-	errmsg := "Couldn't check that VT-X/AMD-v is enabled. Will check that the vm is properly created: %v"
-	content, err := ioutil.ReadFile("/proc/cpuinfo")
-	if err != nil {
-		log.Debugf(errmsg, err)
-		return false
-	}
-
 	for _, v := range features {
-		if bytes.Contains(content, v) {
+		if bytes.Contains(cpuinfo, v) {
 			return false
 		}
 	}
