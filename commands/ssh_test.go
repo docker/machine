0f2ad63ff85d2cc230cb10823dae3f5a43b39eff
Nathan LeClaire <nathan.leclaire@gmail.com>
2015-12-03 19:45:15 -0800

    Add additional tests for SSH
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- commands/ssh_test.go
+++ commands/ssh_test.go
@@ -4,17 +4,36 @@ import (
 	"testing"
 
 	"github.com/docker/machine/commands/commandstest"
+	"github.com/docker/machine/drivers/fakedriver"
 	"github.com/docker/machine/libmachine"
+	"github.com/docker/machine/libmachine/drivers"
+	"github.com/docker/machine/libmachine/host"
 	"github.com/docker/machine/libmachine/libmachinetest"
+	"github.com/docker/machine/libmachine/ssh"
+	"github.com/docker/machine/libmachine/ssh/sshtest"
+	"github.com/docker/machine/libmachine/state"
 	"github.com/stretchr/testify/assert"
 )
 
+type FakeSSHClientCreator struct {
+	client ssh.Client
+}
+
+func (fsc *FakeSSHClientCreator) CreateSSHClient(d drivers.Driver) (ssh.Client, error) {
+	if fsc.client == nil {
+		fsc.client = &sshtest.FakeClient{}
+	}
+	return fsc.client, nil
+}
+
 func TestCmdSSH(t *testing.T) {
 	testCases := []struct {
-		commandLine CommandLine
-		api         libmachine.API
-		expectedErr error
-		helpShown   bool
+		commandLine   CommandLine
+		api           libmachine.API
+		expectedErr   error
+		helpShown     bool
+		clientCreator host.SSHClientCreator
+		expectedShell []string
 	}{
 		{
 			commandLine: &commandstest.FakeCommandLine{
@@ -39,14 +58,54 @@ func TestCmdSSH(t *testing.T) {
 			api:         &libmachinetest.FakeAPI{},
 			expectedErr: ErrExpectedOneMachine,
 		},
+		{
+			commandLine: &commandstest.FakeCommandLine{
+				CliArgs: []string{"default", "df", "-h"},
+			},
+			api: &libmachinetest.FakeAPI{
+				Hosts: []*host.Host{
+					{
+						Name: "default",
+						Driver: &fakedriver.Driver{
+							MockState: state.Running,
+						},
+					},
+				},
+			},
+			expectedErr:   nil,
+			clientCreator: &FakeSSHClientCreator{},
+			expectedShell: []string{"df", "-h"},
+		},
+		{
+			commandLine: &commandstest.FakeCommandLine{
+				CliArgs: []string{"default"},
+			},
+			api: &libmachinetest.FakeAPI{
+				Hosts: []*host.Host{
+					{
+						Name: "default",
+						Driver: &fakedriver.Driver{
+							MockState: state.Stopped,
+						},
+					},
+				},
+			},
+			expectedErr: errStateInvalidForSSH{"default"},
+		},
 	}
 
 	for _, tc := range testCases {
+		host.SetSSHClientCreator(tc.clientCreator)
+
 		err := cmdSSH(tc.commandLine, tc.api)
 		assert.Equal(t, err, tc.expectedErr)
 
 		if fcl, ok := tc.commandLine.(*commandstest.FakeCommandLine); ok {
 			assert.Equal(t, tc.helpShown, fcl.HelpShown)
+		}
+
+		if fcc, ok := tc.clientCreator.(*FakeSSHClientCreator); ok {
+			assert.Equal(t, tc.expectedShell, fcc.client.(*sshtest.FakeClient).ActivatedShell)
 		}
 	}
 }
