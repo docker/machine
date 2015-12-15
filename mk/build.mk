aa83951aad5d612a8d1ae3a5b27f2371c517fd98
David Gageot <david@gageot.net>
2015-12-15 09:12:22 +0100

    Cross build binaries directly in `bin`
    Signed-off-by: David Gageot <david@gageot.net>

--- mk/build.mk
+++ mk/build.mk
@@ -2,7 +2,7 @@ extension = $(patsubst windows,.exe,$(filter windows,$(1)))
 
 define gocross
 	GOOS=$(1) GOARCH=$(2) CGO_ENABLED=0 $(GO) build \
-		-o $(PREFIX)/bin/docker-machine_$(1)-$(2)/docker-$(patsubst cmd/%.go,%,$3)$(call extension,$(GOOS)) \
+		-o $(PREFIX)/bin/docker-$(patsubst cmd/%.go,%,$3)_$(1)-$(2)$(call extension,$(GOOS)) \
 		-a $(VERBOSE_GO) -tags "static_build netgo $(BUILDTAGS)" -installsuffix netgo \
 		-ldflags "$(GO_LDFLAGS) -extldflags -static" $(GO_GCFLAGS) $(3);
 endef
