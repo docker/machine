fd25762c059e72b74ae57c0270d9ac8c8f76cefb
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2015-12-17 13:42:09 +0100

    Add vbox.log to crashreport
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- libmachine/libmachine.go
+++ libmachine/libmachine.go
@@ -98,7 +98,7 @@ func (api *Client) Create(h *host.Host) error {
 	log.Info("Creating machine...")
 
 	if err := api.performCreate(h); err != nil {
-		crashreport.Send(err, "api.performCreate", h.DriverName, "Create")
+		sendCrashReport(err, api, h)
 		return err
 	}
 
@@ -151,4 +151,13 @@ func (api *Client) performCreate(h *host.Host) error {
 
 	return nil
 
+}
+
+func sendCrashReport(err error, api *Client, host *host.Host) {
+	if host.DriverName == "virtualbox" {
+		vboxlogPath := filepath.Join(api.GetMachinesDir(), host.Name, host.Name, "Logs", "VBox.log")
+		crashreport.SendWithFile(err, "api.performCreate", host.DriverName, "Create", vboxlogPath)
+	} else {
+		crashreport.Send(err, "api.performCreate", host.DriverName, "Create")
+	}
 }
