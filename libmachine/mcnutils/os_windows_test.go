a493c789382dafb86b4f243637030840289f785b
David Gageot <david@gageot.net>
2015-12-18 16:26:01 +0100

    FIX #2626 Retrieve windows version on non-english OS
    Signed-off-by: David Gageot <david@gageot.net>

--- libmachine/mcnutils/os_windows_test.go
+++ libmachine/mcnutils/os_windows_test.go
@@ -8,13 +8,10 @@ import (
 
 func TestOSWindows(t *testing.T) {
 	output := `
-Host Name:                 DESKTOP-3A5PULA
-OS Name:                   Microsoft Windows 10 Enterprise
-OS Version:                10.0.10240 N/A Build 10240
-OS Manufacturer:           Microsoft Corporation
-OS Configuration:          Standalone Workstation
-OS Build Type:             Multiprocessor Free
-Registered Owner:          Windows User
+
+Microsoft Windows [version 6.3.9600]
+
 `
-	assert.Equal(t, "10.0.10240 N/A Build 10240", parseOutput(output))
+
+	assert.Equal(t, "Microsoft Windows [version 6.3.9600]", parseOutput(output))
 }
