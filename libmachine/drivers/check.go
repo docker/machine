d38579e36d196dad4e1239f833fff85b2ebca4c4
David Gageot <david@gageot.net>
2015-12-21 14:50:27 +0100

    FIX #2641 Digital Ocean Custom SSH port
    Signed-off-by: David Gageot <david@gageot.net>

--- libmachine/drivers/check.go
+++ libmachine/drivers/check.go
@@ -12,52 +12,58 @@ type CheckDriverOptions struct {
 func (o *CheckDriverOptions) String(key string) string {
 	for _, flag := range o.CreateFlags {
 		if flag.String() == key {
-			_, ok := flag.(mcnflag.StringFlag)
+			f, ok := flag.(mcnflag.StringFlag)
 			if !ok {
 				o.InvalidFlags = append(o.InvalidFlags, flag.String())
 			}
+
+			value, present := o.FlagsValues[key].(string)
+			if present {
+				return value
+			}
+			return f.Value
 		}
 	}
 
-	value, present := o.FlagsValues[key].(string)
-	if present {
-		return value
-	}
 	return ""
 }
 
 func (o *CheckDriverOptions) StringSlice(key string) []string {
 	for _, flag := range o.CreateFlags {
 		if flag.String() == key {
-			_, ok := flag.(mcnflag.StringSliceFlag)
+			f, ok := flag.(mcnflag.StringSliceFlag)
 			if !ok {
 				o.InvalidFlags = append(o.InvalidFlags, flag.String())
 			}
+
+			value, present := o.FlagsValues[key].([]string)
+			if present {
+				return value
+			}
+			return f.Value
 		}
 	}
 
-	value, present := o.FlagsValues[key].([]string)
-	if present {
-		return value
-	}
 	return nil
 }
 
 func (o *CheckDriverOptions) Int(key string) int {
 	for _, flag := range o.CreateFlags {
 		if flag.String() == key {
-			_, ok := flag.(mcnflag.IntFlag)
+			f, ok := flag.(mcnflag.IntFlag)
 			if !ok {
 				o.InvalidFlags = append(o.InvalidFlags, flag.String())
 			}
+
+			value, present := o.FlagsValues[key].(int)
+			if present {
+				return value
+			}
+			return f.Value
 		}
 	}
 
-	value, present := o.FlagsValues[key].(int)
-	if present {
-		return value
-	}
-	return 42
+	return 0
 }
 
 func (o *CheckDriverOptions) Bool(key string) bool {
