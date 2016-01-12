3817b4cc4d71b6940a15f582f46c5a91a8844e2c
Stefan Scherer <scherer_stefan@icloud.com>
2016-01-13 00:46:11 +0100

    Check grand parent if not directly called from a windows shell
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- libmachine/shell/shell_windows_test.go
+++ libmachine/shell/shell_windows_test.go
@@ -13,13 +13,29 @@ func TestDetect(t *testing.T) {
 
 	shell, err := Detect()
 
-	assert.Equal(t, "cmd", shell)
+	assert.Equal(t, "powershell", shell)
 	assert.NoError(t, err)
 }
 
-func TestStartedBy(t *testing.T) {
-	shell, err := startedBy()
+func TestGetNameAndItsPpidOfCurrent(t *testing.T) {
+	shell, shellppid, err := getNameAndItsPpid(os.Getpid())
+
+	assert.Equal(t, "shell.test.exe", shell)
+	assert.Equal(t, os.Getppid(), shellppid)
+	assert.NoError(t, err)
+}
+
+func TestGetNameAndItsPpidOfParent(t *testing.T) {
+	shell, _, err := getNameAndItsPpid(os.Getppid())
 
 	assert.Equal(t, "go.exe", shell)
+	assert.NoError(t, err)
+}
+
+func TestGetNameAndItsPpidOfGrandParent(t *testing.T) {
+	shell, shellppid, err := getNameAndItsPpid(os.Getppid())
+	shell, shellppid, err = getNameAndItsPpid(shellppid)
+
+	assert.Equal(t, "powershell.exe", shell)
 	assert.NoError(t, err)
 }
