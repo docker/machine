ebd24e10477c8fa78bae7f744950f09ca1adabf9
Marconi Moreto Jr <git@marconijr.com>
2016-03-12 12:04:47 +0800

    refactored to use local docker client
    Signed-off-by: Marconi Moreto Jr. <me@marconijr.com>

--- commands/scpi.go
+++ commands/scpi.go
@@ -3,10 +3,10 @@ package commands
 import (
 	"errors"
 	"fmt"
+	"os/exec"
 	"strings"
 
 	"github.com/docker/machine/libmachine"
-	"github.com/docker/machine/libmachine/host"
 )
 
 func cmdScpi(c CommandLine, api libmachine.API) error {
@@ -20,69 +20,94 @@ func cmdScpi(c CommandLine, api libmachine.API) error {
 	dest := args[1]
 	hostInfoLoader := &storeHostInfoLoader{api}
 
-	srcHost, srcImage, err := getScpiHostAndImage(src, true, api)
+	dockerExec, machineExec, err := getDockerAndMachineExecs()
 	if err != nil {
 		return err
 	}
 
-	destHost, _, err := getScpiHostAndImage(dest, false, api)
+	srcHostName, srcImage, err := parseScpiTargetInfo(src, true, hostInfoLoader)
+	if err != nil {
+		return err
+	}
+	srcHostArgs, err := getHostArgs(srcHostName, machineExec)
 	if err != nil {
 		return err
 	}
 
-	// Establish connection to source and destination hosts
-	srcClient, err := srcHost.CreateSSHClient()
+	destHostName, _, err := parseScpiTargetInfo(dest, false, hostInfoLoader)
 	if err != nil {
 		return err
 	}
-	destClient, err := destHost.CreateSSHClient()
+	destHostArgs, err := getHostArgs(destHostName, machineExec)
 	if err != nil {
 		return err
 	}
 
 	saveFilename := generateSaveFilename(srcImage)
-	if err = srcClient.Shell(fmt.Sprintf("docker save -o %s %s", saveFilename, srcImage)); err != nil {
+	if err = saveImage(srcHostArgs, saveFilename, srcImage, dockerExec); err != nil {
 		return err
 	}
 
-	// Copy archive from source to destination
-	cpSrc := fmt.Sprintf("%s:%s", srcHost.Name, saveFilename)
-	cpDest := fmt.Sprintf("%s:%s", destHost.Name, saveFilename)
-	cmd, err := getScpCmd(cpSrc, cpDest, false, hostInfoLoader)
-	if err != nil {
+	if err = loadAndCleanup(destHostArgs, saveFilename, dockerExec); err != nil {
 		return err
 	}
-	if err = runCmdWithStdIo(*cmd); err != nil {
+	return nil
+}
+
+func saveImage(srcHostArgs []string, saveFilename, srcImage, dockerExec string) error {
+	saveCmdArgs := append(srcHostArgs, []string{"save", "-o", saveFilename, srcImage}...)
+	saveCmd := exec.Command(dockerExec, saveCmdArgs...)
+	if err := runCmdWithStdIo(*saveCmd); err != nil {
+		return err
+	}
+	return nil
+}
+
+func loadAndCleanup(destHostArgs []string, saveFilename, dockerExec string) error {
+	// Load image on destination
+	loadCmdArgs := append(destHostArgs, []string{"load", "-i", saveFilename}...)
+	loadCmd := exec.Command(dockerExec, loadCmdArgs...)
+	if err := runCmdWithStdIo(*loadCmd); err != nil {
 		return err
 	}
 
-	// Restore image on destination
-	if err = destClient.Shell(fmt.Sprintf("docker load -i %s", saveFilename)); err != nil {
+	// Cleanup
+	rmCmd := exec.Command("rm", saveFilename)
+	if err := runCmdWithStdIo(*rmCmd); err != nil {
 		return err
 	}
-
-	// cleanup
-	rmCmd := fmt.Sprintf("rm %s", saveFilename)
-	srcClient.Shell(rmCmd)
-	destClient.Shell(rmCmd)
 
 	return nil
 }
 
-func getScpiHostAndImage(hostAndPath string, isSource bool, api libmachine.API) (*host.Host, string, error) {
+func getDockerAndMachineExecs() (string, string, error) {
+	dockerExec, err := exec.LookPath("docker")
+	if err != nil {
+		return "", "", err
+	}
+
+	machineExec, err := exec.LookPath("docker-machine")
+	if err != nil {
+		return "", "", err
+	}
+
+	return dockerExec, machineExec, nil
+}
+
+func parseScpiTargetInfo(hostAndImage string, isSource bool, hostInfoLoader HostInfoLoader) (string, string, error) {
 	var (
-		hostName = hostAndPath
+		hostName = hostAndImage
 		image    string
 	)
 
 	if isSource {
 		// Validate path. e.g. "default:redis:2.8.23"
-		if !strings.Contains(hostAndPath, ":") {
-			return nil, "", errors.New("Invalid source image")
+		if !strings.Contains(hostAndImage, ":") {
+			return "", "", errors.New("Invalid source image")
 		}
 
 		// Split hostname and image name
-		parts := strings.SplitN(hostAndPath, ":", 2)
+		parts := strings.SplitN(hostAndImage, ":", 2)
 		hostName = parts[0]
 		image = parts[1]
 
@@ -91,13 +116,21 @@ func getScpiHostAndImage(hostAndPath string, isSource bool, api libmachine.API) 
 		}
 	}
 
-	// Remote path
-	host, err := api.Load(hostName)
-	if err != nil {
-		return nil, "", fmt.Errorf("Error loading host: %s", err)
+	// Validate host
+	if _, err := hostInfoLoader.load(hostName); err != nil {
+		return "", "", fmt.Errorf("Error loading host: %s", err)
 	}
 
-	return host, image, nil
+	return hostName, image, nil
+}
+
+func getHostArgs(hostName, machineExec string) ([]string, error) {
+	// Host flags
+	rawHostArgs, err := exec.Command(machineExec, "config", hostName).Output()
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(rawHostArgs), " "), nil
 }
 
 func generateSaveFilename(srcImage string) string {
--- commands/scpi_test.go
+++ commands/scpi_test.go
@@ -3,63 +3,53 @@ package commands
 import (
 	"testing"
 
-	"github.com/docker/machine/drivers/fakedriver"
-	"github.com/docker/machine/libmachine"
-	"github.com/docker/machine/libmachine/host"
-	"github.com/docker/machine/libmachine/libmachinetest"
-	"github.com/docker/machine/libmachine/state"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetScpiHostAndImage(t *testing.T) {
+func TestParseScpiTargetInfo(t *testing.T) {
 	redisImage := "redis:2.8.23"
 	redisLatestImage := "redis:latest"
 	defautRedisSrc := "default:" + redisImage
 	defautRedisLatestSrc := "default:" + redisLatestImage
+	somevm := "somevm"
 
-	defaultHost := &host.Host{
-		Name: "default",
-		Driver: &fakedriver.Driver{
-			MockState: state.Stopped,
-		},
-	}
-	destvmHost := &host.Host{
-		Name: "destvm",
-		Driver: &fakedriver.Driver{
-			MockState: state.Stopped,
-		},
-	}
-
-	api := &libmachinetest.FakeAPI{
-		Hosts: []*host.Host{defaultHost, destvmHost},
-	}
+	hostInfoLoader := &MockHostInfoLoader{MockHostInfo{
+		sshKeyPath: "/fake/keypath/id_rsa",
+	}}
 
 	testCases := []struct {
-		src      string
-		isSource bool
-		api      libmachine.API
-		host     *host.Host
-		image    string
+		srcOrDest      string
+		isSource       bool
+		hostInfoLoader HostInfoLoader
+		hostName       string
+		image          string
 	}{
 		{
-			src:      defautRedisSrc,
-			isSource: true,
-			api:      api,
-			host:     defaultHost,
-			image:    redisImage,
+			srcOrDest:      defautRedisSrc,
+			isSource:       true,
+			hostInfoLoader: hostInfoLoader,
+			hostName:       "default",
+			image:          redisImage,
 		},
 		{
-			src:      defautRedisLatestSrc,
-			isSource: true,
-			api:      api,
-			host:     defaultHost,
-			image:    redisLatestImage,
+			srcOrDest:      defautRedisLatestSrc,
+			isSource:       true,
+			hostInfoLoader: hostInfoLoader,
+			hostName:       "default",
+			image:          redisLatestImage,
+		},
+		{
+			srcOrDest:      somevm,
+			isSource:       false,
+			hostInfoLoader: hostInfoLoader,
+			hostName:       "somevm",
+			image:          "",
 		},
 	}
 
 	for _, tc := range testCases {
-		host, image, err := getScpiHostAndImage(tc.src, tc.isSource, tc.api)
-		assert.Equal(t, host, tc.host)
+		hostName, image, err := parseScpiTargetInfo(tc.srcOrDest, tc.isSource, tc.hostInfoLoader)
+		assert.Equal(t, hostName, tc.hostName)
 		assert.Equal(t, image, tc.image)
 		assert.Equal(t, err, nil)
 	}
