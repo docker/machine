be8f469a828ffd6ad8ca0baaef017c49e3bab713
Maksim Malchuk <maksim.malchuk@gmail.com>
2016-04-12 11:43:18 +0300

    Correct error handling for external SSH client
    In some cases, (e.g. private key not accessible or has incorrect
    permissions) docker-machine failed with error "Something went wrong
    running an SSH command!". This commit will add the correct debug
    messages and show the correct errors for the bad private keys.
    Also, due to incorrect handling POSIX file permissions in Windows
    some checks should be ignored.
    
    Signed-off-by: Maksim Malchuk <maksim.malchuk@gmail.com>

--- libmachine/ssh/client_test.go
+++ libmachine/ssh/client_test.go
@@ -1,6 +1,7 @@
 package ssh
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -41,5 +42,43 @@ func TestGetSSHCmdArgs(t *testing.T) {
 	for _, c := range cases {
 		cmd := getSSHCmd(c.binaryPath, c.args...)
 		assert.Equal(t, cmd.Args, c.expectedArgs)
+	}
+}
+
+func TestNewExternalClient(t *testing.T) {
+	cases := []struct {
+		sshBinaryPath string
+		user          string
+		host          string
+		port          int
+		auth          *Auth
+		expectedError string
+		skipOS        string
+	}{
+		{
+			sshBinaryPath: "/usr/local/bin/ssh",
+			user:          "docker",
+			host:          "localhost",
+			port:          22,
+			auth:          &Auth{Keys: []string{"/tmp/private-key-not-exist"}},
+			expectedError: "stat /tmp/private-key-not-exist: no such file or directory",
+			skipOS:        "none",
+		},
+		{
+			sshBinaryPath: "/usr/local/bin/ssh",
+			user:          "docker",
+			host:          "localhost",
+			port:          22,
+			auth:          &Auth{Keys: []string{"/dev/null"}},
+			expectedError: "Permissions 0410000666 for '/dev/null' are too open.",
+			skipOS:        "windows",
+		},
+	}
+
+	for _, c := range cases {
+		if runtime.GOOS != c.skipOS {
+			_, err := NewExternalClient(c.sshBinaryPath, c.user, c.host, c.port, c.auth)
+			assert.EqualError(t, err, c.expectedError)
+		}
 	}
 }
