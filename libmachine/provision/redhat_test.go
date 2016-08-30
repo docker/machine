ab7d860660383a9d1c4076a7d55710c543453cbf
tronicum <tronicum@gmail.com>
2016-08-30 15:47:57 +0200

    removed the yum repo setup code. it duplicates code logic now within install.sh of get.docker.com @ https://github.com/docker/docker/blob/master/hack/install.sh and is currently not called anymore, too.
    Signed-off-by: tronicum <tronicum@gmail.com>

--- libmachine/provision/redhat_test.go
+++ libmachine/provision/redhat_test.go
@@ -11,28 +11,6 @@ import (
 	"github.com/docker/machine/libmachine/swarm"
 )
 
-func TestRedHatGenerateYumRepoList(t *testing.T) {
-	info := &OsRelease{
-		ID: "rhel",
-	}
-	p := NewRedHatProvisioner("rhel", nil)
-	p.SetOsReleaseInfo(info)
-
-	buf, err := generateYumRepoList(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	m, err := regexp.MatchString(".*centos/7.*", buf.String())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !m {
-		t.Fatalf("expected match for centos/7")
-	}
-}
-
 func TestRedHatDefaultStorageDriver(t *testing.T) {
 	p := NewRedHatProvisioner("", &fakedriver.Driver{})
 	p.SSHCommander = provisiontest.NewFakeSSHCommander(provisiontest.FakeSSHCommanderOptions{})
