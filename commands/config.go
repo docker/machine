d8884192a2e85875b3c4771a41027cda9923a674
Nathan LeClaire <nathan.leclaire@gmail.com>
2016-12-07 14:19:15 -0800

    Fix 'docker-machine config' cert paths
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- commands/config.go
+++ commands/config.go
@@ -3,7 +3,9 @@ package commands
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/docker/machine/commands/mcndirs"
 	"github.com/docker/machine/libmachine"
 	"github.com/docker/machine/libmachine/check"
 	"github.com/docker/machine/libmachine/log"
@@ -24,15 +26,21 @@ func cmdConfig(c CommandLine, api libmachine.API) error {
 		return err
 	}
 
-	dockerHost, authOptions, err := check.DefaultConnChecker.Check(host, c.Bool("swarm"))
+	dockerHost, _, err := check.DefaultConnChecker.Check(host, c.Bool("swarm"))
 	if err != nil {
 		return fmt.Errorf("Error running connection boilerplate: %s", err)
 	}
 
 	log.Debug(dockerHost)
 
+	tlsCACert := filepath.Join(mcndirs.GetMachineDir(), host.Name, "ca.pem")
+	tlsCert := filepath.Join(mcndirs.GetMachineDir(), host.Name, "cert.pem")
+	tlsKey := filepath.Join(mcndirs.GetMachineDir(), host.Name, "key.pem")
+
+	// TODO(nathanleclaire): These magic strings for the certificate file
+	// names should be cross-package constants.
 	fmt.Printf("--tlsverify\n--tlscacert=%q\n--tlscert=%q\n--tlskey=%q\n-H=%s\n",
-		authOptions.CaCertPath, authOptions.ClientCertPath, authOptions.ClientKeyPath, dockerHost)
+		tlsCACert, tlsCert, tlsKey, dockerHost)
 
 	return nil
 }
