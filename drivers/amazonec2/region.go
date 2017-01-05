9635a1e6826c1f642f3459b1249806149a07ddea
Jerome Loisel <loisel.jerome@gmail.com>
2017-01-05 17:30:23 +0100

    Amazon: support London (eu-west-2)
    Add Support for new Amazon Region (London), and upgrade Ubuntu 16.04 LTS AMIs to latest available version.
    
    Signed-off-by: jloisel <loisel.jerome@gmail.com>

--- drivers/amazonec2/region.go
+++ drivers/amazonec2/region.go
@@ -8,24 +8,26 @@ type region struct {
 	AmiId string
 }
 
-// Release 16.04 LTS 20161013
+// Ubuntu 16.04 LTS 20161221 hvm:ebs-ssd (amd64)
 // See https://cloud-images.ubuntu.com/locator/ec2/
 var regionDetails map[string]*region = map[string]*region{
-	"ap-northeast-1":  {"ami-31892c50"},
-	"ap-northeast-2":  {"ami-a3915acd"},
-	"ap-southeast-1":  {"ami-18e7417b"},
-	"ap-southeast-2":  {"ami-7be4d618"},
-	"ap-south-1":      {"ami-7e94fe11"},
-	"ca-central-1":    {"ami-ca6ddfae"},
-	"cn-north-1":      {"ami-d7c511ba"},
-	"eu-central-1":    {"ami-597c8236"},
+	"ap-northeast-1":  {"ami-18afc47f"},
+	"ap-northeast-2":  {"ami-93d600fd"},
+	"ap-southeast-1":  {"ami-87b917e4"},
+	"ap-southeast-2":  {"ami-e6b58e85"},
+	"ap-south-1":      {"ami-dd3442b2"},
+	"ca-central-1":    {"ami-7112a015"},
+	"cn-north-1":      {"ami-31499d5c"},
+	"eu-central-1":    {"ami-fe408091"},
+	"eu-west-1":       {"ami-ca80a0b9"},
+	"eu-west-2":       {"ami-ede2e889"},
 	"eu-west-1":       {"ami-c593deb6"},
-	"sa-east-1":       {"ami-909b06fc"},
-	"us-east-1":       {"ami-fd6e3bea"},
-	"us-east-2":       {"ami-0a104a6f"},
-	"us-west-1":       {"ami-73531b13"},
-	"us-west-2":       {"ami-f1ca1091"},
-	"us-gov-west-1":   {"ami-8df24aec"},
+	"sa-east-1":       {"ami-e075ed8c"},
+	"us-east-1":       {"ami-9dcfdb8a"},
+	"us-east-2":       {"ami-fcc19b99"},
+	"us-west-1":       {"ami-b05203d0"},
+	"us-west-2":       {"ami-b2d463d2"},
+	"us-gov-west-1":   {"ami-19d56d78"},
 	"custom-endpoint": {""},
 }
 
