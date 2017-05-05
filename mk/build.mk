705c3683ec9ef3ba18613f0d0808affa405bece6
Sven Dowideit <SvenDowideit@home.org.au>
2017-05-05 12:52:49 +1000

    Use PKG_NAME in mk so it can be reused in drivers
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- mk/build.mk
+++ mk/build.mk
@@ -16,7 +16,7 @@ define gocross
 	$(if $(findstring [$(1)/$(2)],$(VALID_OS_ARCH)), \
 	GOOS=$(1) GOARCH=$(2) CGO_ENABLED=0 \
 		$(GO) build \
-		-o $(PREFIX)/bin/docker-machine-${os.$(1)}-${arch.$(2)}$(call extension,$(GOOS)) \
+		-o $(PREFIX)/bin/$(PKG_NAME)-${os.$(1)}-${arch.$(2)}$(call extension,$(GOOS)) \
 		-a $(VERBOSE_GO) -tags "static_build netgo $(BUILDTAGS)" -installsuffix netgo \
 		-ldflags "$(GO_LDFLAGS) -extldflags -static" $(GO_GCFLAGS) ./cmd/machine.go;)
 endef
@@ -27,10 +27,10 @@ build-clean:
 build-x: $(shell find . -type f -name '*.go')
 	$(foreach GOARCH,$(TARGET_ARCH),$(foreach GOOS,$(TARGET_OS),$(call gocross,$(GOOS),$(GOARCH))))
 
-$(PREFIX)/bin/docker-machine$(call extension,$(GOOS)): $(shell find . -type f -name '*.go')
+$(PREFIX)/bin/$(PKG_NAME)$(call extension,$(GOOS)): $(shell find . -type f -name '*.go')
 	$(GO) build \
 	-o $@ \
 	$(VERBOSE_GO) -tags "$(BUILDTAGS)" \
 	-ldflags "$(GO_LDFLAGS)" $(GO_GCFLAGS) ./cmd/machine.go
 
-build: $(PREFIX)/bin/docker-machine$(call extension,$(GOOS))
+build: $(PREFIX)/bin/$(PKG_NAME)$(call extension,$(GOOS))
