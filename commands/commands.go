6f7dd33a5947c2d6e5593271a6141099641d15e0
Konstantin Gribov <grossws@gmail.com>
2017-07-31 16:56:44 +0300

    Fix misleading `create` command description
    Signed-off-by: Konstantin Gribov <grossws@gmail.com>

--- commands/commands.go
+++ commands/commands.go
@@ -221,7 +221,7 @@ var Commands = []cli.Command{
 		Flags:           SharedCreateFlags,
 		Name:            "create",
 		Usage:           "Create a machine",
-		Description:     fmt.Sprintf("Run '%s create --driver name' to include the create flags for that driver in the help text.", os.Args[0]),
+		Description:     fmt.Sprintf("Run '%s create --driver name --help' to include the create flags for that driver in the help text.", os.Args[0]),
 		Action:          runCommand(cmdCreateOuter),
 		SkipFlagParsing: true,
 	},
