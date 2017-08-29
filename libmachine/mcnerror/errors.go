9f9e463bf7ecfd2aeb1d0b576c3384bfbde68b63
Victoria Bialas <victoria.bialas@docker.com>
2017-08-29 15:38:35 -0700

    added escaped quotes around machine names
    Signed-off-by: Victoria Bialas <victoria.bialas@docker.com>

--- libmachine/mcnerror/errors.go
+++ libmachine/mcnerror/errors.go
@@ -17,7 +17,7 @@ type ErrHostDoesNotExist struct {
 }
 
 func (e ErrHostDoesNotExist) Error() string {
-	return fmt.Sprintf("Docker machine %q does not exist", e.Name)
+	return fmt.Sprintf("Docker machine \"%q\" does not exist", e.Name)
 }
 
 type ErrHostAlreadyExists struct {
@@ -25,7 +25,7 @@ type ErrHostAlreadyExists struct {
 }
 
 func (e ErrHostAlreadyExists) Error() string {
-	return fmt.Sprintf("Docker machine %q already exists", e.Name)
+	return fmt.Sprintf("Docker machine \"%q\" already exists", e.Name)
 }
 
 type ErrDuringPreCreate struct {
