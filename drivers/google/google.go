0ed3d445804a480a17b0f5d02d4b7c707e3cc40d
Filip Weiss <me@fiws.net>
2017-09-09 12:24:41 +0200

    google driver: remove 404 machines with warning
    Signed-off-by: Filip Weiss <me@fiws.net>

--- drivers/google/google.go
+++ drivers/google/google.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/http"
 	"strings"
 
 	"github.com/docker/machine/libmachine/drivers"
@@ -11,6 +12,8 @@ import (
 	"github.com/docker/machine/libmachine/mcnflag"
 	"github.com/docker/machine/libmachine/ssh"
 	"github.com/docker/machine/libmachine/state"
+
+	"google.golang.org/api/googleapi"
 )
 
 // Driver is a struct compatible with the docker.hosts.drivers.Driver interface.
@@ -396,7 +399,15 @@ func (d *Driver) Remove() error {
 	}
 
 	if err := c.deleteInstance(); err != nil {
-		return err
+		if googleErr, ok := err.(*googleapi.Error); ok {
+			if googleErr.Code == http.StatusNotFound {
+				log.Warn("Remote instance does not exist, proceeding with removing local reference")
+			} else {
+				return err
+			}
+		} else {
+			return err
+		}
 	}
 
 	return c.deleteDisk()
