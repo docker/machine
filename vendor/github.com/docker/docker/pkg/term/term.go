c89fcd5b5ffdbcb0b884f2ca68984400c3b27250
Ivan Markin <sw@nogoegst.net>
2018-02-12 23:54:10 +0000

    Override docker vendor constraint to master
    Signed-off-by: Ivan Markin <sw@nogoegst.net>

--- vendor/github.com/docker/docker/pkg/term/term.go
+++ vendor/github.com/docker/docker/pkg/term/term.go
@@ -1,23 +1,30 @@
 // +build !windows
 
-package term
+// Package term provides structures and helper functions to work with
+// terminal (state, sizes).
+package term // import "github.com/docker/docker/pkg/term"
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"os"
 	"os/signal"
-	"syscall"
-	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 var (
+	// ErrInvalidState is returned if the state of the terminal is invalid.
 	ErrInvalidState = errors.New("Invalid terminal state")
 )
 
+// State represents the state of the terminal.
 type State struct {
 	termios Termios
 }
 
+// Winsize represents the size of the terminal window.
 type Winsize struct {
 	Height uint16
 	Width  uint16
@@ -25,23 +32,20 @@ type Winsize struct {
 	y      uint16
 }
 
-func GetWinsize(fd uintptr) (*Winsize, error) {
-	ws := &Winsize{}
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(ws)))
-	// Skipp errno = 0
-	if err == 0 {
-		return ws, nil
-	}
-	return ws, err
+// StdStreams returns the standard streams (stdin, stdout, stderr).
+func StdStreams() (stdIn io.ReadCloser, stdOut, stdErr io.Writer) {
+	return os.Stdin, os.Stdout, os.Stderr
 }
 
-func SetWinsize(fd uintptr, ws *Winsize) error {
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSWINSZ), uintptr(unsafe.Pointer(ws)))
-	// Skipp errno = 0
-	if err == 0 {
-		return nil
+// GetFdInfo returns the file descriptor for an os.File and indicates whether the file represents a terminal.
+func GetFdInfo(in interface{}) (uintptr, bool) {
+	var inFd uintptr
+	var isTerminalIn bool
+	if file, ok := in.(*os.File); ok {
+		inFd = file.Fd()
+		isTerminalIn = IsTerminal(inFd)
 	}
-	return err
+	return inFd, isTerminalIn
 }
 
 // IsTerminal returns true if the given file descriptor is a terminal.
@@ -50,8 +54,8 @@ func IsTerminal(fd uintptr) bool {
 	return tcget(fd, &termios) == 0
 }
 
-// Restore restores the terminal connected to the given file descriptor to a
-// previous state.
+// RestoreTerminal restores the terminal connected to the given file descriptor
+// to a previous state.
 func RestoreTerminal(fd uintptr, state *State) error {
 	if state == nil {
 		return ErrInvalidState
@@ -62,6 +66,7 @@ func RestoreTerminal(fd uintptr, state *State) error {
 	return nil
 }
 
+// SaveState saves the state of the terminal connected to the given file descriptor.
 func SaveState(fd uintptr) (*State, error) {
 	var oldState State
 	if err := tcget(fd, &oldState.termios); err != 0 {
@@ -71,9 +76,11 @@ func SaveState(fd uintptr) (*State, error) {
 	return &oldState, nil
 }
 
+// DisableEcho applies the specified state to the terminal connected to the file
+// descriptor, with echo disabled.
 func DisableEcho(fd uintptr, state *State) error {
 	newState := state.termios
-	newState.Lflag &^= syscall.ECHO
+	newState.Lflag &^= unix.ECHO
 
 	if err := tcset(fd, &newState); err != 0 {
 		return err
@@ -82,6 +89,9 @@ func DisableEcho(fd uintptr, state *State) error {
 	return nil
 }
 
+// SetRawTerminal puts the terminal connected to the given file descriptor into
+// raw mode and returns the previous state. On UNIX, this puts both the input
+// and output into raw mode. On Windows, it only puts the input into raw mode.
 func SetRawTerminal(fd uintptr) (*State, error) {
 	oldState, err := MakeRaw(fd)
 	if err != nil {
@@ -91,13 +101,24 @@ func SetRawTerminal(fd uintptr) (*State, error) {
 	return oldState, err
 }
 
+// SetRawTerminalOutput puts the output of terminal connected to the given file
+// descriptor into raw mode. On UNIX, this does nothing and returns nil for the
+// state. On Windows, it disables LF -> CRLF translation.
+func SetRawTerminalOutput(fd uintptr) (*State, error) {
+	return nil, nil
+}
+
 func handleInterrupt(fd uintptr, state *State) {
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, os.Interrupt)
-
 	go func() {
-		_ = <-sigchan
-		RestoreTerminal(fd, state)
-		os.Exit(0)
+		for range sigchan {
+			// quit cleanly and the new terminal item is on a new line
+			fmt.Println()
+			signal.Stop(sigchan)
+			close(sigchan)
+			RestoreTerminal(fd, state)
+			os.Exit(1)
+		}
 	}()
 }
