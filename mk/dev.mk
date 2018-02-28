1d0786b7cb6238c0898bb107b3ded65478157462
Ivan Markin <sw@nogoegst.net>
2018-02-28 13:22:30 +0000

    Remove godep references in Makefiles
    Signed-off-by: Ivan Markin <sw@nogoegst.net>

--- mk/dev.mk
+++
@@ -1,9 +0,0 @@
-dep-save:
-	$(if $(GODEP), , \
-		$(error Please install godep: go get github.com/tools/godep))
-	$(GODEP) save $(shell go list ./... | grep -v vendor/)
-
-dep-restore:
-	$(if $(GODEP), , \
-		$(error Please install godep: go get github.com/tools/godep))
-	$(GODEP) restore -v
