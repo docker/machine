30fd79214f53c6d9e0d96d7fa8791bb7a11a66b5
Joshua Welsh <joshua.welsh@performancehorizon.com>
2018-04-19 12:24:48 +0100

    EC2: Added Ubuntu 16.04 20180405 EBS and Instance Store AMIs
    Also added the ap-northeast-3 region.
    
    Note: The cn-northwest-1 region is no longer listed on the
    Ubuntu AMI locator, therefore the images for this region have
    not been changed and there is no instance store AMI defined.
    
    Signed-off-by: Joshua Welsh <joshua.welsh@performancehorizon.com>

--- drivers/amazonec2/region.go
+++ drivers/amazonec2/region.go
@@ -5,31 +5,33 @@ import (
 )
 
 type region struct {
-	AmiId string
+	AmiId              string
+	InstanceStoreAmiId string
 }
 
-// Ubuntu 16.04 LTS 20180228.1 hvm:ebs-ssd (amd64)
+// Ubuntu 16.04 LTS 20180405 hvm:ebs-ssd + hvm:instance-store (amd64)
 // See https://cloud-images.ubuntu.com/locator/ec2/
 var regionDetails map[string]*region = map[string]*region{
-	"ap-northeast-1":  {"ami-bcb7f6da"},
-	"ap-northeast-2":  {"ami-5073de3e"},
-	"ap-southeast-1":  {"ami-41e4af3d"},
-	"ap-southeast-2":  {"ami-c1498fa3"},
-	"ap-south-1":      {"ami-1083dc7f"},
-	"ca-central-1":    {"ami-8d9e19e9"},
-	"cn-north-1":      {"ami-cc4499a1"}, // Note: this is 20180126
-	"cn-northwest-1":  {"ami-fd0e1a9f"}, // Note: this is 20180126
-	"eu-central-1":    {"ami-bc4925d3"},
-	"eu-west-1":       {"ami-0b541372"},
-	"eu-west-2":       {"ami-ff46a298"},
-	"eu-west-3":       {"ami-9465d3e9"},
-	"sa-east-1":       {"ami-b5501bd9"},
-	"us-east-1":       {"ami-927185ef"},
-	"us-east-2":       {"ami-b9daeddc"},
-	"us-west-1":       {"ami-264c4646"},
-	"us-west-2":       {"ami-78a22900"},
-	"us-gov-west-1":   {"ami-2561ea44"},
-	"custom-endpoint": {""},
+	"ap-northeast-1":  {"ami-60a4b21c", "ami-55564729"},
+	"ap-northeast-2":  {"ami-633d920d", "ami-c63e91a8"},
+	"ap-northeast-3":  {"ami-89c3cdf4", "ami-b4c2ccc9"},
+	"ap-southeast-1":  {"ami-82c9ecfe", "ami-c4d7f2b8"},
+	"ap-southeast-2":  {"ami-2b12dc49", "ami-7e2ee01c"},
+	"ap-south-1":      {"ami-dba580b4", "ami-bba287d4"},
+	"ca-central-1":    {"ami-9d7afcf9", "ami-24048240"},
+	"cn-north-1":      {"ami-a209d6cf", "ami-4f06d922"},
+	"cn-northwest-1":  {"ami-fd0e1a9f", ""}, // Note: this is 20180126
+	"eu-central-1":    {"ami-cd491726", "ami-59421cb2"},
+	"eu-west-1":       {"ami-74e6b80d", "ami-85e5bbfc"},
+	"eu-west-2":       {"ami-506e8f37", "ami-7e739219"},
+	"eu-west-3":       {"ami-9a03b5e7", "ami-9b0fb9e6"},
+	"sa-east-1":       {"ami-5782d43b", "ami-9385d3ff"},
+	"us-east-1":       {"ami-6dfe5010", "ami-bbcf61c6"},
+	"us-east-2":       {"ami-e82a1a8d", "ami-d82a1abd"},
+	"us-west-1":       {"ami-493f2f29", "ami-4b21312b"},
+	"us-west-2":       {"ami-ca89eeb2", "ami-2c95f254"},
+	"us-gov-west-1":   {"ami-fb77e29a", "ami-7f70e51e"},
+	"custom-endpoint": {"", ""},
 }
 
 func awsRegionsList() []string {
