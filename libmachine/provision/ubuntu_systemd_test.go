ea7d4b7716c217d42352621cd3bb4aea4e35ec85
Tianon Gravi <admwiggin@gmail.com>
2018-08-13 12:58:51 -0700

    Update "aufs" to "overlay2" in several places
    Signed-off-by: Tianon Gravi <admwiggin@gmail.com>

--- libmachine/provision/ubuntu_systemd_test.go
+++ libmachine/provision/ubuntu_systemd_test.go
@@ -38,7 +38,7 @@ func TestUbuntuSystemdDefaultStorageDriver(t *testing.T) {
 	p := NewUbuntuSystemdProvisioner(&fakedriver.Driver{}).(*UbuntuSystemdProvisioner)
 	p.SSHCommander = provisiontest.NewFakeSSHCommander(provisiontest.FakeSSHCommanderOptions{})
 	p.Provision(swarm.Options{}, auth.Options{}, engine.Options{})
-	if p.EngineOptions.StorageDriver != "aufs" {
-		t.Fatal("Default storage driver should be aufs")
+	if p.EngineOptions.StorageDriver != "overlay2" {
+		t.Fatal("Default storage driver should be overlay2")
 	}
 }
