1879869875d51cec643157c62860d9435abf0421
Gennady Lipenkov <xgen@yandex-team.ru>
2019-04-22 17:43:20 +0300

    Support set Platform ID
    Signed-off-by: Gennady Lipenkov <xgen@yandex-team.ru>

--- drivers/yandex/yandex.go
+++ drivers/yandex/yandex.go
@@ -218,6 +218,12 @@ func (d *Driver) GetCreateFlags() []mcnflag.Flag {
 			Name:   "yandex-userdata",
 			Usage:  "Path to file with cloud-init user-data",
 		},
+		mcnflag.StringFlag{
+			EnvVar: "YC_PLATFORM_ID",
+			Name:   "yandex-platform-id",
+			Usage:  "ID of the hardware platform configuration for the instance",
+			Value:  defaultPlatformID,
+		},
 	}
 
 }
@@ -395,6 +401,7 @@ func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
 	d.ImageID = flags.String("yandex-image-id")
 	d.Labels = flags.StringSlice("yandex-labels")
 	d.Memory = flags.Int("yandex-memory")
+	d.PlatformID = flags.String("yandex-platform-id")
 	d.Preemptible = flags.Bool("yandex-preemptible")
 	d.SSHUser = flags.String("yandex-ssh-user")
 	d.SSHPort = flags.Int("yandex-ssh-port")
