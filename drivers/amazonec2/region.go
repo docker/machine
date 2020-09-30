35a0a8f30fca45e091f9fd530047f2271373e4e0
mqhub <mquial@gmail.com>
2020-09-30 09:59:50 -0400

    Update region.go
    Add AWS region us-gov-east-1

--- drivers/amazonec2/region.go
+++ drivers/amazonec2/region.go
@@ -30,6 +30,7 @@ var regionDetails map[string]*region = map[string]*region{
 	"us-west-1":       {"ami-264c4646"},
 	"us-west-2":       {"ami-78a22900"},
 	"us-gov-west-1":   {"ami-2561ea44"},
+	"us-gov-east-1":   {"ami-7609e007"}, // Note: this is 20200814
 	"custom-endpoint": {""},
 }
 
